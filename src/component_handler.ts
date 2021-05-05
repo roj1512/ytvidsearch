9e5f7769a2072d5c591c331eb96391cc68ef3996
Roj Serbest <rojserbest@gmail.com>
2021-05-05 16:18:17 +0300

    video comonent

---
+++ src/component_handler.ts
@@ -0,0 +1,61 @@
+import { getValue } from "./helpers";
+import { video } from "./constants";
+
+export const getVideoComponent = (element) => {
+  const _video = element[video];
+  const component = {
+    _: video,
+    id: getValue(_video, ["videoId"]),
+    title: getValue(_video, ["title", "runs", 0, "text"]),
+    publishedTime: getValue(_video, ["publishedTimeText", "simpleText"]),
+    duration: getValue(_video, ["lengthText", "simpleText"]),
+    viewCount: {
+      text: getValue(_video, ["viewCountText", "simpleText"]),
+      short: getValue(_video, ["shortViewCountText", "simpleText"]),
+    },
+    thumbnails: getValue(_video, ["thumbnail", "thumbnails"]),
+    richThumbnail: getValue(_video, [
+      "richThumbnail",
+      "movingThumbnailRenderer",
+    ]),
+    descriptionSnippet: getValue(_video, ["descriptionSnippet", "runs"]),
+    channel: {
+      name: getValue(_video, "ownerText", "runs", 0, "text"),
+      id: getValue(_video, [
+        "ownerText",
+        "runs",
+        0,
+        "navigationEndpoint",
+        "browserEndpoint",
+        "browseId",
+      ]),
+      thumbnails: getValue(_video, [
+        "channelThumbnailSupportedRenderers",
+        "channelThumbnailWithLinkRenderer",
+        "thumbnail",
+        "thumbnails",
+      ]),
+      accessibility: {
+        title: getValue(_video, [
+          "title",
+          "accessibility",
+          "accessibilityData",
+          "label",
+        ]),
+        duration: getValue(_video, [
+          "lengthText",
+          "accessibility",
+          "accessibilityData",
+          "label",
+        ]),
+      },
+    },
+  };
+
+  component["url"] = `https://youtu.be/${component.id}`;
+  component.channel[
+    "url"
+  ] = `https://www.youtube.com/channel/${component.channel.id}`;
+
+  return component;
+};
